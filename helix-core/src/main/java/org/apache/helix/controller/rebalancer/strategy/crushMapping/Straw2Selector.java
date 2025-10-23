abc3484bd0eab1da5b4044aa8c76a373dfddd77d
Anubhav Agarwal <anubagarwal@linkedin.com>
2025-10-23 09:58:47 +0530

    improving test case and addressing review comments

--- helix-core/src/main/java/org/apache/helix/controller/rebalancer/strategy/crushMapping/Straw2Selector.java
+++ helix-core/src/main/java/org/apache/helix/controller/rebalancer/strategy/crushMapping/Straw2Selector.java
@@ -45,7 +45,7 @@ class Straw2Selector implements Selector {
 
   private double weightedScore(Node child, long input, long round) {
     long hash = _hashFunction.hash(input, child.getId(), round);
-    hash = (hash&0xffff);
+    hash = hash&0xffff;
     // To avoid doing Math.log of 0 changing hash range from [0..65535] to [1..65535]
     hash = hash == 0 ? 1 : hash;
     if (child.getWeight() > 0) {
